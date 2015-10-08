f1c24e8d513d603bfbe1111e5d3be73fb4a4afa8
Josh Phillips <joshuaronan85@yahoo.co.uk>
2015-10-08 16:06:09 +0100

    more  tests

--- test/backendtests.js
+++ test/backendtests.js
@@ -6,3 +6,14 @@ test("check back end test set-up, 1 equals 1", function(t) {
   t.equal(1, 1, "success!");
   t.end();
 });
+test("server sends a response", function(t) {
+  var req = {
+    method: "GET",
+    url: '/meows'
+  };
+  shot.inject(serverFile.handler, req, function(res) {
+
+    t.equal(!!res.responseText, true, "success!");
+    t.end();
+  });
+});
